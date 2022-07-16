6e05efe9b8e79c5257cba4c43120bfb572b203c5
admin <admin@admins-iMac.local>
2022-07-16 13:50:47 -0700

    Clock markers are created and positioned around stopwatch center

--- lib/ui/stopwatch_renderer.dart
+++ lib/ui/stopwatch_renderer.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:stopwatch_flutter/ui/clock_hand.dart';
+import 'package:stopwatch_flutter/ui/clock_markers.dart';
 import 'package:stopwatch_flutter/ui/elapsed_time_text.dart';
 
 class StopwatchRenderer extends StatelessWidget {
@@ -16,6 +17,22 @@ class StopwatchRenderer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: [
+        for (var i = 0; i < 60; i++)
+          Positioned(
+            left: radius,
+            top: radius,
+            child: ClockSecondsTickMarker(seconds: i, radius: radius),
+          ),
+        for (var i = 5; i <= 60; i += 5)
+          Positioned(
+            top: radius,
+            left: radius,
+            child: ClockTextMarker(
+              value: i,
+              maxValue: 60,
+              radius: radius,
+            ),
+          ),
         Positioned(
           left: radius,
           top: radius,
